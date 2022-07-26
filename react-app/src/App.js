e16846d0cf11641c9b3d6b4597ad0c04f8c01345
Elisa <elisa.torelli1@gmail.com>
2022-07-26 14:32:16 +0200

    add code snippet

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -11,8 +11,8 @@ function App() {
   return (
     <div className="App">
       {/* {signin ? <p>Sei registrato</p> : <SignInCard /> } */}
-      {/* <SignInCard /> */}
-      <LogInCard />
+      <SignInCard />
+      {/* <LogInCard /> */}
     </div>
   );
 }
